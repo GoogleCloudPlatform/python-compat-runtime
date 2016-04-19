af78d4b05d1f3ac3d029871bf27b84a3bccdbac6
Bryan Mau <bryanmau1@gmail.com>
2016-04-19 13:57:21 -0700

    Add NDB test

---
+++ tests/e2e/tests/ndb_test.py
@@ -0,0 +1,21 @@
+import os
+
+from google.appengine.ext.ndb import model, tasklets
+
+class Employee(model.Model):
+  name = model.StringProperty()
+  age = model.IntegerProperty()
+
+
+@pytest.fixture
+def ctx():
+  ctx = tasklets.get_context()
+  ctx.set_cache_policy(False)
+  ctx.set_memcache_policy(False)
+  return ctx
+
+def test_basics(ctx):
+  worker = Employee(name='Alice', age=55)
+  key = worker.put()
+  same_worker = key.get()
+  assert worker == same_worker
